95c00b0626991171c673676cf9228a408d952daf
Ubuntu <ubuntu@ip-172-31-36-130.us-west-2.compute.internal>
2013-07-10 18:59:34 +0000

    Update grader.json to take a URL as an input

--- grader.js
+++ grader.js
@@ -22,10 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLURL_DEFAULT = "http://still-meadow-6022.herokuapp.com/";
 
 var assertFileExists = function(infile) {
   var instr = infile.toString();
@@ -33,6 +35,11 @@ var assertFileExists = function(infile) {
     console.log("%s dose not exist. Exiting.", instr);
     process.exit(); // http://nodejs.org/api/process.html#process_process_exit_code
   }
+  return instr;
+};
+
+var assertUrlExists = function(inurl) {
+  var instr = rest.get(inurl);
   return instr;
 };
 
@@ -66,6 +73,7 @@ if (require.main == module) {
   program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <html_url>', 'Html from URL', clone(assertUrlExists), HTMLURL_DEFAULT)
     .parse(process.argv);
     
   var checkJson = checkHtmlFile(program.file, program.checks);
